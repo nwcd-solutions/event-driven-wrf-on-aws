b0f188c45739775d432f84f165fa14cef5f39c6c
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-06-25 07:26:02 +0000

    Update sf.py

--- src/wx/sf.py
+++ src/wx/sf.py
@@ -1,3 +1,95 @@
+from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    aws_lambda as λ,
+    aws_lambda_event_sources as λ_events,
+    aws_logs as logs,
+    aws_s3 as s3,
+    aws_s3_assets as assets,
+    aws_s3_notifications,
+    aws_sns as sns,
+    aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as tasks,
+    Aws, CfnOutput, Duration, Fn, NestedStack, Tags
+)
+from constructs import Construct
+
+class stepfunction (NestedStack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id)
+
+        vpc = kwargs["vpc"]
+        policy_doc = iam.PolicyDocument(statements=[
+            iam.PolicyStatement(
+                actions=["execute-api:Invoke", "execute-api:ManageConnections"],
+                resources=["arn:aws:execute-api:*:*:*"],
+                effect=iam.Effect.ALLOW),
+            iam.PolicyStatement(
+                actions=["states:*"],
+                resources=["*"],
+                effect=iam.Effect.ALLOW),
+            iam.PolicyStatement(
+                actions=["iam:*"],
+                resources=["*"],
+                effect=iam.Effect.ALLOW),
+            iam.PolicyStatement(
+                actions=["cloudformation:*"],
+                resources=["*"],
+                effect=iam.Effect.ALLOW),
+        ])
+        lambda_role = iam.Role(self, "Role",
+                assumed_by=iam.CompositePrincipal(
+                    iam.ServicePrincipal("lambda.amazonaws.com"),
+                    iam.ServicePrincipal("sts.amazonaws.com"),
+                ),
+                description="CreateAPILambdaRole",
+                managed_policies=[
+                    iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
+                    iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaVPCAccessExecutionRole"),
+                    ],
+                inline_policies={"cluster_lambda": policy_doc},
+        )
+
+        subnet = vpc.public_subnets[1].subnet_id
+        for net in vpc.public_subnets:
+            if net.availability_zone == "us-east-2b":
+                subnet = net
+
+        layer = λ.LayerVersion(self, "lambda_layer",
+                compatible_runtimes=[λ.Runtime.PYTHON_3_9],
+                code=λ.Code.from_asset("./layer.zip"),
+                layer_version_name="wx_layer",
+                description="WX Lambda Layer",
+            )
+
+        cluster_lambda = λ.Function(self, "lambda_func_cluster",
+                code=λ.Code.from_asset("./lambda"),
+                environment={
+                    "BUCKET_NAME": bucket_name,
+                    "CLUSTER_NAME": cluster_name,
+                    "JWTKEY": jwt_key,
+                    "PCLUSTER_API_URL": purl,
+                    "REGION": Aws.REGION,
+                    "FORECAST_TMPL": forecast_tmpl,
+                    "S3_URL_POST_INSTALL_HEADNODE": f"{post_head_amd64.s3_object_url}",
+                    "SG": sg_rds.security_group_id,
+                    "SNS_TOPIC": sns_topic,
+                    "SM_ARN": sm.state_machine_arn,
+                    "SUBNETID": subnet,
+                },
+                handler="cluster.main",
+                layers=[layer],
+                log_retention=logs.RetentionDays.ONE_DAY,
+                role=lambda_role,
+                runtime=λ.Runtime.PYTHON_3_9,
+                timeout=Duration.seconds(60)
+            )
+        Tags.of(cluster_lambda).add("Purpose", "Event Driven Weather Forecast", priority=300)     
+        sm = sfn.StateMachine(self, "WXStateMachine",
+            definition=definition,
+            timeout=Duration.minutes(65))
+
+        Tags.of(sm).add("Purpose", "Event Driven Weather Forecast", priority=300)
 destroy_sf_def= {
   "Comment": "A description of my state machine",
   "StartAt": "destroy cluster",
