a56a023c4188e288bb89fe355fc8985cca31c8b2
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-06-25 11:14:05 +0000

    Update sf.py

--- src/wx/sf.py
+++ src/wx/sf.py
@@ -90,12 +90,233 @@ class stepfunction (NestedStack):
             definition_string=json.dumps(destroy_sf_def),
             timeout=Duration.minutes(65))
 
-        create_sf_def= 
+        create_sf_def= {
+            "Comment": "state machine to create cluster",
+            "StartAt": "destroy cluster",
+            "States": {
+                "destroy cluster": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::lambda:invoke",
+                    "OutputPath": "$.Payload",
+                    "Parameters": {
+                        "Payload.$": "$",
+                        "FunctionName": "arn:aws:lambda:us-east-2:880755836258:function:WX-clusterNestedStackclus-lambdafuncdestroy811B2A7-3cxqzYGHhxQz:$LATEST"
+      },
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "Lambda.ServiceException",
+            "Lambda.AWSLambdaException",
+            "Lambda.SdkClientException",
+            "Lambda.TooManyRequestsException"
+          ],
+          "IntervalSeconds": 2,
+          "MaxAttempts": 6,
+          "BackoffRate": 2
+        }
+      ],
+      "Next": "Whether cluster could be deleted"
+    },
+    "Whether cluster could be deleted": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Variable": "$.cluster.clusterStatus",
+          "StringEquals": "DELETE_IN_PROGRESS",
+          "Next": "Wait for cluster deleting"
+        }
+      ],
+      "Default": "Fail"
+    },
+    "Wait for cluster deleting": {
+      "Type": "Wait",
+      "Seconds": 60,
+      "Next": "Monitor deleting status"
+    },
+    "Monitor deleting status": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::lambda:invoke",
+      "Parameters": {
+        "Payload.$": "$",
+        "FunctionName": "arn:aws:lambda:us-east-2:880755836258:function:WX-clusterNestedStackclus-lambdafuncdestroy811B2A7-3cxqzYGHhxQz:$LATEST"
+      },
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "Lambda.ServiceException",
+            "Lambda.AWSLambdaException",
+            "Lambda.SdkClientException",
+            "Lambda.TooManyRequestsException"
+          ],
+          "IntervalSeconds": 2,
+          "MaxAttempts": 6,
+          "BackoffRate": 2
+        }
+      ],
+      "Next": "Whether Success Deleted",
+      "OutputPath": "$.Payload"
+    },
+    "Whether Success Deleted": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Variable": "$.cluster.clusterStatus",
+          "StringEquals": "DELETE_COMPLETE",
+          "Next": "Success"
+        },
+        {
+          "Variable": "$.cluster.clusterStatus",
+          "StringEquals": "DELETE_IN_PROGRESS",
+          "Next": "Wait for cluster deleting"
+        }
+      ],
+      "Default": "Fail"
+    },
+    "Fail": {
+      "Type": "Fail"
+    },
+    "Success": {
+      "Type": "Succeed"
+    }
+  }
+}
+        
         create_sf = sfn.StateMachine(self, "WX_createStateMachine",
             definition_string=json.dumps(create_sf_def),
             timeout=Duration.minutes(65))
         
-        main_sf_def = 
+        main_sf_def = {
+            "Comment": "main state machine workflow",
+            "StartAt": "Create single OD Cluster",
+            "States": {
+                "Create single OD Cluster": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::states:startExecution.sync:2",
+                    "Parameters": {
+                        "StateMachineArn": "arn:aws:states:us-east-2:880755836258:stateMachine:ClusterCreate",
+                        "Input": {
+                            "action": "create",
+                            "type": "od",
+                            "AWS_STEP_FUNCTIONS_STARTED_BY_EXECUTION_ID.$": "$$.Execution.Id"
+                        }
+                    },
+                    "OutputPath": "$.Output",
+                    "Next": "Choice (1)"
+                },
+                "Choice (1)": {
+                    "Type": "Choice",
+                    "Choices": [
+                        {
+                            "Variable": "$.clusterStatus",
+                            "StringEquals": "CREATE_COMPLETE",
+                            "Next": "Publish completed message (1)"
+                        }
+                    ],
+                    "Default": "Publish Fail to create cluster message (1)"
+                },
+                "Publish Fail to create cluster message (1)": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::sns:publish",
+                    "Parameters": {
+                        "TopicArn": "arn:aws:sns:us-east-2:880755836258:WRF",
+                        "Message": {
+                            "message": "Clusters failed to be created complete, will start to destroy the cf",
+                            "detail information": {
+                                "OD cluster": {
+                                    "cluster name": "$[0].clusterName"
+                                },
+                                "Spot cluster": {
+                                    "cluster name": "$[1].clusterName"
+                                }
+                            }
+                        }
+                    },
+                    "ResultPath": null,
+                    "Next": "Destroy single Cluster"
+                },
+                "Publish completed message (1)": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::sns:publish",
+                    "Parameters": {
+                        "TopicArn": "arn:aws:sns:us-east-2:880755836258:WRF",
+                        "Message": {
+                            "message": "Clusters have been created successfully, will start to run forecast jobs",
+                            "detail information": {
+                                "OD cluster": {
+                                    "cluster name": "$[0].clusterName",
+                                    "head node": "$[0].headNode"
+                                },
+                                "Spot cluster": {
+                                    "cluster name": "$[1].clusterName",
+                                    "head node": "$[1].headNode"
+                                }
+                             }
+                        }
+                    },
+                    "ResultPath": null,
+                    "Next": "Pass (1)"
+                },
+                "Pass (1)": {
+                    "Type": "Pass",
+                    "Next": "Destroy single Cluster"
+                },
+                "Destroy single Cluster": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::states:startExecution.sync:2",
+                    "Parameters": {
+                        "StateMachineArn": "arn:aws:states:us-east-2:880755836258:stateMachine:ClusterDestroy",
+                        "Input": {
+                            "action": "destroy",
+                            "clusterName.$": "$.clusterName",
+                            "region.$": "$.region",
+                            "AWS_STEP_FUNCTIONS_STARTED_BY_EXECUTION_ID.$": "$$.Execution.Id"
+                        }
+                    },
+                    "Next": "Choice (2)"
+                },
+                "Choice (2)": {
+                    "Type": "Choice",
+                    "Choices": [
+                      {
+                        "Variable": "$.Output.cluster.clusterStatus",
+                        "StringEquals": "DELETE_COMPLETE",
+                        "Next": "Destroy failed notification (1)"
+                      }
+                    ],
+                    "Default": "Destroy complete notification (1)"
+                },
+                "Destroy complete notification (1)": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::sns:publish",
+                    "Parameters": {
+                        "TopicArn": "arn:aws:sns:us-east-2:880755836258:WRF",
+                        "Message": {
+                            "message": "Clusters has been deleted"
+                        }
+                    },
+                    "Next": "Success",
+                    "ResultPath": null
+                },
+                "Destroy failed notification (1)": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::sns:publish",
+                    "Parameters": {
+                        "TopicArn": "arn:aws:sns:us-east-2:880755836258:WRF",
+                        "Message": {
+                            "message": "Clusters failed to be deleted"
+                        }
+                    },
+                    "Next": "Fail",
+                    "ResultPath": null
+                },
+                "Fail": {
+                    "Type": "Fail"
+                },
+                "Success": {
+                    "Type": "Succeed"
+                }
+            }
+        }
         main_sf = sfn.StateMachine(self, "WX_mainStateMachine",
             definition_string=json.dumps(main_sf_def),
             timeout=Duration.minutes(65))
