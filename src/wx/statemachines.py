069be4801be75174f4c9efefa6dfa4fb3a1b2de4
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-11-21 11:00:10 +0000

    Update statemachines.py

--- src/wx/statemachines.py
+++ src/wx/statemachines.py
@@ -148,7 +148,7 @@ class stepfunction (NestedStack):
                     "SUBNETID": subnet,
                     "FORECAST_DAYS": forecast_days,
                     "NUM_DOMAINS": domains,
-                    "KMS_POLICY": f"Policy:arn:aws:iam::{Aws.ACCOUNT}:{kms_all_policy.policy_name}",
+                    "KMS_POLICY": kms_all_policy.managed_policy_arn,
                     "DYNAMODB": para_db.table_name,
                 },
                 handler="cluster.main",
