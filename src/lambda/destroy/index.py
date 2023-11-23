e88b0accbd7495c1c0f979d660d50a409dc6032f
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-11-23 07:22:41 +0000

    Create index.py

---
+++ src/lambda/destroy/index.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+import os
+import re
+from datetime import datetime
+
+ssm = boto3.client('ssm')
+dynamodb = boto3.resource('dynamodb')
+
+def handler(event, context):
+    print(event)
+    response = ssm.get_parameter(Name=os.getenv('FTIME'))
+    ftime = response['Parameter']['Value']    
+    response = ssm.get_parameter(Name= os.getenv('EXEC_ID'))
+    id = response['Parameter']['Value']    
+    clustername=os.getenv('CLUSTER_NAME')
+    region=os.getenv('REGION')
+    sfn = boto3.client('stepfunctions')
+    sfn.start_execution(
+        stateMachineArn=os.getenv("SM_ARN"),
+        input = "{\"action\" : \"destroy\",\"clusterName\":\""+clustername+ "\",\"region\":\""+region+"\",\"ftime\":\""+ftime + "\",\"id\":\""+id + "\"}"
+    )
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    exec_table = dynamodb.Table(os.getenv('EXEC_DB'))
+    exec_table.update_item(
+        Key={
+            'ftime':ftime,
+            'id': id
+        },
+        UpdateExpression = 'SET job_finished_time = :job_finished_time',
+        ExpressionAttributeValues = {
+            :job_finished_time':current_time,
+        }
+    )   
