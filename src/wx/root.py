c5c04f0a41e9ffacfa2641582ead9a8c8109d879
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-11-25 05:36:43 +0000

    Update root.py

--- src/wx/root.py
+++ src/wx/root.py
@@ -36,7 +36,7 @@ class Root(Stack):
             sf.add_dependency(slurmdb)
         sf.add_dependency(vpc)
        
-        api = ApiGateway(self,"api",datastore=datastore,bucket=bucket_name.value_as_string)
+        api = ApiGateway(self,"api",para_db=datastore.para_db,bucket=bucket_name.value_as_string)
         api.add_dependency(sf)
 
 
