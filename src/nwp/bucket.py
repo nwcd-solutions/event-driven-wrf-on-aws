a17eba1d04cc8a47e04cfa92110ccd0d2b260aee
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-12-07 13:37:09 +0000

    Create bucket.py

---
+++ src/nwp/bucket.py
@@ -0,0 +1,26 @@
+from aws_cdk import (
+    aws_s3 as s3,
+    CfnOutput, NestedStack, Tags
+)
+from constructs import Construct
+import random
+import string
+
+def generate_bucket_name():
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(10))
+
+class Bucket(NestedStack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id)
+
+        self.bucket = s3.Bucket(
+            self, 'NWPBucket',
+            bucket_name='nwp-',
+            removal_policy=core.RemovalPolicy.DESTROY,
+        )
+
+    @property
+    def outputs(self):
+        return  self.bucket.bucket_name
