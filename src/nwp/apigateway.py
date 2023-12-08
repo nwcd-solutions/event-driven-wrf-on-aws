01c28df95f525f21ea3f3f893718ccf08bd3722d
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-12-08 08:32:07 +0000

    Update apigateway.py

--- src/nwp/apigateway.py
+++ src/nwp/apigateway.py
@@ -54,6 +54,7 @@ class ApiGateway(NestedStack):
                 domain_prefix = domain_name
             )
         )
+        cognito_domain = apply_removal_policy(core.RemovalPolicy.DESTROY)
         #---------------------------------------------------------------------------------------------
         # Create domain service Lambda functions
         #---------------------------------------------------------------------------------------------
