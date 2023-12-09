cfc8535c91322b99e13d37ec8d56b06e3b5ec57f
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-12-09 04:32:24 +0000

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -15,8 +15,8 @@ cd ..
 while true; do
     read -p "Do you want to use slurm accounting? (yes/no): " slurm_acct
     case $slurm_acct in
-        [Yy]*) slurm_acct = true; break;;
-        [Nn]*|"") slurm_acct = false; break;;
+        [Yy]*) slurm_acct=true; break;;
+        [Nn]*|"") slurm_acct=false; break;;
         *) echo "Please answer yes or no.";;
     esac
 done
@@ -24,4 +24,10 @@ done
 #read -p "Please enter the admin username" username
 
 cdk bootstrap
-cdk deploy
+cdk_out=$(cdk deploy)
+cognito_userpool_id=$(echo "$cdk_output" | grep "WRF.cognitouserpoolid" | awk '{print $2}')
+cognito_client_id=$(echo "$cdk_output" | grep "WRF.cognitoclientid" | awk '{print $2}')
+echo $cognito_userpool_id
+echo $cognito_client_id
+sed -i "s/<aws_user_pools_id>/$cognito_userpool_id/g" console/src/aws-export.js
+sed -i "s/<aws_user_pools_web_client_id>/$cognito_client_id/g" console/src/aws-export.js
