88306a66312dbe4bd68affbd7de5f121e4b3081a
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-12-09 06:19:11 +0000

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -34,10 +34,10 @@ apigw_name=$(jq '.WRF.apigwname' outputs.json)
 location_map_name=$(jq '.WRF.locationmapname' outputs.json)
 echo $cognito_userpool_id
 echo $cognito_client_id
-sed -i "s/<aws_user_pools_id>/$cognito_userpool_id/g" console/src/aws-export.js
-sed -i "s/<aws_user_pools_web_client_id>/$cognito_client_id/g" console/src/aws-export.js
-sed -i "s/<aws_user_pools_web_client_id>/$cognito_domain/g" console/src/aws-export.js
-sed -i "s/<aws_user_pools_web_client_id>/$apigw_endpoint/g" console/src/aws-export.js
-sed -i "s/<aws_user_pools_web_client_id>/$apigw_name/g" console/src/aws-export.js
-sed -i "s/<aws_user_pools_web_client_id>/$location_map_name/g" console/src/aws-export.js
+#sed -i "s/<aws_user_pools_id>/$cognito_userpool_id/g" console/src/aws-export.js
+#sed -i "s/<aws_user_pools_web_client_id>/$cognito_client_id/g" console/src/aws-export.js
+#sed -i "s/<aws_user_pools_web_client_id>/$cognito_domain/g" console/src/aws-export.js
+#sed -i "s/<aws_user_pools_web_client_id>/$apigw_endpoint/g" console/src/aws-export.js
+#sed -i "s/<aws_user_pools_web_client_id>/$apigw_name/g" console/src/aws-export.js
+#sed -i "s/<aws_user_pools_web_client_id>/$location_map_name/g" console/src/aws-export.js
 rm outputs.json layer.zip
