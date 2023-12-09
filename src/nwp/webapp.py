987df91e96dd8ce70f6c3c02c1c350f893de20cc
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-12-09 08:05:07 +0000

    Update webapp.py

--- src/nwp/webapp.py
+++ src/nwp/webapp.py
@@ -19,7 +19,7 @@ class WebApp(NestedStack):
             self, 'AmplifyReactTestRepo',
             repository_name='nwp-solution-repo',
             description='CodeCommit repository that will be used as the source repository for the sample react app and the cdk app',
-            code=codecommit.Code.from_directory("console/", "develop")
+            code=codecommit.Code.from_directory("build/", "develop")
         )
         # Part 2 - Creation of the Amplify Application
         #amplify_app = amplify.CfnApp(
