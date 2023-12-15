15d73b152455c11cc5124654b29cdee66d3c48a2
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-12-15 09:04:25 +0000

    Update MiniStatistics.tsx

--- front-end/console/src/Pages/dashboard/MiniStatistics.tsx
+++ front-end/console/src/Pages/dashboard/MiniStatistics.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Card } from "@aws-amplify/ui-react";
+import { Card,Loader } from "@aws-amplify/ui-react";
 
 interface MiniStatisticProps {
   title: string;
   amount: string;
   icon?: JSX.Element;
-  percentage?: number;
+  loading: boolean;
 }
 
 const MiniStatistics = (props: MiniStatisticProps) => {
@@ -13,7 +13,11 @@ const MiniStatistics = (props: MiniStatisticProps) => {
     <Card height="100%" borderRadius="15px" className="bg-gradient-red">
       <div className="card-content">
         <div className="card-title"> {props.title} </div>
-        <div className="card-statistics-amount">{props.amount}</div>
+        {props.loading?
+          <div className="card-statistics-amount"><Loader></Loader></div>
+        :
+          <div className="card-statistics-amount">{props.amount}</div>
+        }
         <div className="card-statistics-icon">{props.icon}</div>
       </div>
     </Card>
