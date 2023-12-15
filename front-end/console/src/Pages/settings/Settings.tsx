067e5f56de2be7fc0da027228e63385ae077642d
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-12-15 11:19:05 +0000

    Update Settings.tsx

--- front-end/console/src/Pages/settings/Settings.tsx
+++ front-end/console/src/Pages/settings/Settings.tsx
@@ -1,13 +1,9 @@
 import React ,{ useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { View, Heading, ScrollView ,Divider,SwitchField,Text,Flex,Button,Loader } from "@aws-amplify/ui-react";
-import ParaTable from "./ParameterList";
+import { View, Heading, ScrollView ,Text,Flex,Button,Loader } from "@aws-amplify/ui-react";
 import DomainTable from "./DomainList";
-import { Parameter } from "./Parameter"
-import  EditParameter from "./ParameterEdit";
 import { Domain } from "./Domain"
 import { get,del,put } from 'aws-amplify/api';
-import Forms from '../domainDetail';
 import { Modal } from 'react-bootstrap';
 interface SettingsProps {
   getToken:()=>void;
@@ -16,40 +12,14 @@ interface SettingsProps {
 const Settings = ({getToken}:SettingsProps) => {
 
   const navigate = useNavigate();
-  const [parameters, setParameters] = useState<Parameter[]>([]);
-  const [parameter , setParameter] =useState<Parameter>();
+
   const [loading, setLoading] = useState(false);
   const [deleting, setDeleting] = useState(false)
   const [error, setError] = useState<string | undefined>(undefined);
   const [domains, setDomains] = useState<Domain[]>([]);
   const [domainName, setDomainName] =useState<string | undefined>(undefined);
-  const [mode_type,setMode_type] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false)
-  const [showUpdateModal, setShowUpdateModal] = useState(false)
 
-  async function updateParameter(name:string,value:string){
-    console.log('parameter: ', name,value);
-    try{
-      put({
-        apiName: 'WrfAPIGateway',
-        path: '/parameter/',              
-        options: {
-          headers: {
-            Authorization: await getToken()!,
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-          },
-          body: {
-            "name":name,
-            "value":value
-          },
-        }
-      });
-      
-    }catch (err) {
-      console.log('PUT call failed: ', err);
-    }
-  }
   async function loadDomains() {
     setLoading(true);
     
@@ -82,49 +52,10 @@ const Settings = ({getToken}:SettingsProps) => {
       setLoading(false);
     }
   }
-  async function loadParameters() {
-    setLoading(true);
-    try {
-     
-      console.log('get token: ', await getToken());
-      const { body } = await get({
-        apiName: 'WrfAPIGateway',
-        path: '/parameter/',
-        options: {
-          headers: {
-            Authorization: await getToken()!,
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-          },
-          queryParams: {
-            "all":"true"//name: 'shandong'
-          }
-        }
-      }).response;
-      const result=await body.json()  as { [key: string]: any };
-      const parameters:Parameter[]= result["para"];
-      const auto_mode:string=result["auto"]["value"];
-      console.log('print response:',auto_mode)
-      let mode_type:boolean=false;
-      setMode_type(auto_mode === "True");
-      setError('');
-      setParameters(parameters);
-      
-    }
-    catch (e) {
-      if (e instanceof Error) {
-        setError(e.message);
-        console.log(e)
-      }
-    } finally {
-      setLoading(false);
-    }
-  }
 
   async function deleteDomain(name:string) {
     setLoading(true);
-    try {
-     
+    try {    
       console.log('get token: ', await getToken());
       const response = await del({
         apiName: 'WrfAPIGateway',
@@ -161,37 +92,9 @@ const Settings = ({getToken}:SettingsProps) => {
   }
 
   useEffect(() => {   
-    loadParameters();
     loadDomains();
   },[]);
   
-  const handleModeChange = (e:boolean) => {
-        if(e){
-          setMode_type(true);          
-          updateParameter("/event-driven-wrf/auto_mode","True");                 
-        }else{
-          setMode_type(false);          
-          updateParameter("/event-driven-wrf/auto_mode","False");                        
-        }
-        
-  };
-
-  const handleParameterEditClose = () => {
-    setShowUpdateModal(false);
-  };
-  const handleEditParameter = (parameter:Parameter) => {
-    setParameter(parameter);
-    console.log('parameter before save: ', parameter);
-    setShowUpdateModal(true);
-  };
-  const handleSaveParameter = (parameter:Parameter) => {
-    setParameter(parameter);
-    console.log('parameter after save: ', parameter);
-    updateParameter(parameter.description,parameter.value);
-    loadParameters();
-    setParameter({ ...parameter });
-    setShowUpdateModal(false);
-  };
   const handleDeleteDomain =(name:string) => {    
     setShowDeleteModal(true);
     setDomainName(name);
@@ -202,7 +105,7 @@ const Settings = ({getToken}:SettingsProps) => {
   return (
     <>
       <View padding="1rem">
-        <Heading color="#333" level={5} > Parameters Setting </Heading>
+        <Heading color="#333" level={5} > Domain Configuration </Heading>
       </View>
       
       <View
@@ -212,25 +115,7 @@ const Settings = ({getToken}:SettingsProps) => {
         padding="1rem"
         minHeight="80vh"
       >
-        <Heading color="#333"> General Setting </Heading>
-        <br></br>
-        <SwitchField
-          isDisabled={false}
-          label="Auto Enabled"
-          labelPosition="start"
-          isChecked={mode_type}
-          onChange={(e) => handleModeChange(e.target.checked)}
-        />
-        <br></br>
-        <br></br>
-        <ScrollView width="100%">        
-          <ParaTable parameters= { parameters } onEdit = { handleEditParameter }/>
-        </ScrollView>
-        <br></br>
-        <Divider orientation="horizontal" />
-        <br></br>
-
-        <Heading color="#333"  > Domain Setting </Heading>
+        
       { loading ? 
         <Loader 
             size="large"
@@ -243,20 +128,6 @@ const Settings = ({getToken}:SettingsProps) => {
       </View>
       {/* if parameter is not null*/}
 
-      { parameter &&
-      <Modal show={showUpdateModal} onHide={handleParameterEditClose} >
-        <View
-          backgroundColor="var(--amplify-colors-white)"
-          borderRadius="6px"
-          maxWidth={{ base: "100%", large: "100%" }}
-          padding="1rem"
-        >
-          <Heading color="#333"> Modify Parameter</Heading>
-          <br></br>
-          <EditParameter parameter={parameter} onCancel={handleParameterEditClose} onSave={handleSaveParameter}/>
-        </View> 
-      </Modal>
-      }
       { domainName &&
       <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)} >
         <View
