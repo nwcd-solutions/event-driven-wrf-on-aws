fd6197372d7a3514984862bc80d2ae7096f2d523
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-12-18 09:38:37 +0000

    Update aws-export.js

--- front-end/aws-export.js
+++ front-end/aws-export.js
@@ -4,6 +4,7 @@
 const config = {
     "aws_project_region": "<aws_region>",
     "aws_cognito_region": "<aws_region>",
+    "aws_cognito_identity_pool_id": <cognito_identity_pool_id>,
     "aws_user_pools_id": <aws_user_pools_id>,
     "aws_user_pools_web_client_id": <aws_user_pools_web_client_id>,
     "oauth": {
