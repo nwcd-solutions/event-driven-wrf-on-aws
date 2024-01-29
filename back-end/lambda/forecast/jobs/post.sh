c328fcac405ed1b90848983d04a07dac5badd61a
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2024-01-29 14:35:53 +0000

    Create post.sh

---
+++ back-end/lambda/forecast/jobs/post.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+echo 'begin to perform post handling'
+
+conda activate yunda-python39
+# Base dependencies have installed on AMI already
+# if new dependencies are required, please install with conda command,like
+# conda install xxx=1.2.3
